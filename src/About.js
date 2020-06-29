a62ad902b04c2d982b9045f4a38ca2910ebc7d9a
Danimal92 <daniel.zaltsman92@gmail.com>
2020-06-28 18:02:28 -0700

    Added styling to about page

--- src/About.js
+++ src/About.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const About = () => {
+
+
+    let style = {
+        background: "#2B2D42",
+        border: '#ef233c',
+    }
+
+
     return (
         <h5>
             Inspired by a friend to make questions for trivia night between friends. <br/> This site uses Opentdb to source questions and generate trivia.
