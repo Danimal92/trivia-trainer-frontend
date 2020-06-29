04530e90f50ffe7ecd063a6fa77f932cc429048c
Danimal92 <daniel.zaltsman92@gmail.com>
2020-06-28 18:28:51 -0700

    Styling updates about page

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ const About = () => {
     let style = {
         background: "#2B2D42",
         border: '1px solid #ef233c',
-        padding: '50px'
+        padding: '40px'
     }
 
 
@@ -16,7 +16,7 @@ const About = () => {
         <Container style={style}>
             <Row className='justify-content-center' >
                     <h5>
-                        Inspired by a friend to make questions for trivia night between friends. <br/> This site uses Opentdb to source questions and generate trivia.
+                        Inspired by a friend to make questions for trivia night between friends. <br/> This site uses Opentdb to source questions and generate trivia. Enjoy!
                     </h5>
             </Row>
         </Container>
