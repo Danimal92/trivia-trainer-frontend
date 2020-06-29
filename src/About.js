07e7557018a4621dc5e86c6f248375d269444498
Danimal92 <daniel.zaltsman92@gmail.com>
2020-06-28 18:15:47 -0700

    Improved styling on about page

--- src/About.js
+++ src/About.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 const About = () => {
 
 
     let style = {
         background: "#2B2D42",
-        border: '#ef233c',
+        border: '1px solid #ef233c',
+        padding: '50px'
     }
 
 
     return (
-        <h5>
-            Inspired by a friend to make questions for trivia night between friends. <br/> This site uses Opentdb to source questions and generate trivia.
-        </h5>
+        <Container style={style}>
+            <Row className='justify-content-center' >
+                    <h5>
+                        Inspired by a friend to make questions for trivia night between friends. <br/> This site uses Opentdb to source questions and generate trivia.
+                    </h5>
+            </Row>
+        </Container>
     )
 }
  export default About
